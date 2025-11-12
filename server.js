46daa34861c0ad53e01c5eb634647f65e2bf7a70
Andrey Kan <list@livewings.com>
2025-11-12 10:35:08 -0500

    chrome flags

--- server.js
+++ server.js
@@ -4,19 +4,21 @@ var prerender = require('./lib');
 var server = prerender({
     pageLoadTimeout: 1 * 2000,
     chromeLocation: '/usr/bin/chromium-browser',
+    /*
     chromeFlags: [
-        //'--disable-extensions',
+        '--disable-extensions',
         '--no-sandbox',
         '--headless=new',
         '--disable-gpu',
-        //'--disable-software-rasterizer',
+        '--disable-software-rasterizer',
         '--hide-scrollbars',
-        //'--remote-debugging-port=9222',
+        '--remote-debugging-port=9222',
         '--skip-js-errors',
-        //'--disable-dev-shm-usage',
+        '--disable-dev-shm-usage',
         '--enable-logging',
         '--v=1'
     ],
+     */
     workers: 4,
     forwardHeaders: true,
     logRequests: true,
